7192b78e5dc34e8d1057bac37f78ebbb4e382eb2
Xarblu <xarblu@protonmail.com>
2026-02-23 19:28:06 +0100

    bcachefs: unconditional module version
    Signed-off-by: Xarblu <xarblu@protonmail.com>

---
+++ fs/bcachefs/module-version.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * bcachefs module version information.
+ *
+ * Copyright 2010, 2011 Kent Overstreet <kent.overstreet@gmail.com>
+ * Copyright 2012 Google, Inc.
+ */
+
+#include <linux/module.h>
+
+MODULE_VERSION("%%VERSION_STRING%%");
